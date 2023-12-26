48314489535b58892fffb34556f90b1b407933b2
zapytalsyavsebe <154928566+pivpavOP@users.noreply.github.com>
2023-12-27 00:26:41 +0300

    Delete laba3/src directory

--- laba3/src/EldersSpecialisation.java
+++
@@ -1,7 +0,0 @@
-public enum EldersSpecialisation {
-    Shaman("Шаман"),DeafaultElder("Обычный старец");
-    private String translation;
-    EldersSpecialisation(String translation){
-        this.translation = translation;
-    }
-}
--- laba3/src/Interfaces/ShoggotsList.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-import Classes.Shoggot;
-
-public interface ShoggotsList {
-    void add(Shoggot shoggot);
-    boolean removeInd(int index);
-    Shoggot getIndex(int index);
-}
